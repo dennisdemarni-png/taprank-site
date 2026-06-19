96bbdc73b5c47971e4866a7d22857b20e4eaabb1
dennisdemarni-png <dennisdemarni@gmail.com>
2026-06-19 03:26:46 +0100

    Create demo.jsx

---
+++ pages/pages/r/demo.jsx
@@ -0,0 +1,12 @@
+export async function getServerSideProps() {
+  return {
+    redirect: {
+      destination: "https://www.google.com/search?q=google+review",
+      permanent: false
+    }
+  };
+}
+
+export default function DemoRedirect() {
+  return null;
+}
